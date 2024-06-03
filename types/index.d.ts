f26aa2e8f1c7bd5456d842423cd6f00c404774f8
kenneth <kennethbelardo55@gmail.com>
2024-06-03 13:25:57 +0800

    chore: Fix jsDoc

--- types/index.d.ts
+++ types/index.d.ts
@@ -5,25 +5,22 @@ declare module "tralsedb-express-mysql" {
   } from "mysql2/promise";
   import { Request, Response, NextFunction } from "express";
 
-  import { DatabaseInstance } from "./tralse";
-
   export * from "./tralse";
 
   export interface MySequelizePool extends Pool {}
   export interface Connection extends MySequelizeConnection {}
 
+  /**
+   * Middleware to attach TralseMySQL to requests.
+   *
+   * @param {import('mysql2/promise').Pool} pool - The database connection pool.
+   * @param {string} dbName - The name of the database.
+   * @param {boolean} [enableTransactions=false] - Whether to enable transaction support.
+   * @returns {Function} - The middleware function.
+   */
   export const TralseMySQL: (
     pool: MySequelizePool,
     dbName: string,
     enableTransactions?: boolean
   ) => (req: Request, res: Response, next: NextFunction) => Promise<void>;
-
-  export const initializeDatabase: (
-    req: Request,
-    pool: MySequelizePool,
-    dbName: string,
-    enableTransactions: boolean
-  ) => Promise<DatabaseInstance>;
-
-  export function log(dbName: string, message: string): void;
 }
--- types/tralse.d.ts
+++ types/tralse.d.ts
@@ -1,5 +1,4 @@
 import { Pool, PoolConnection as MySequelizeConnection } from "mysql2/promise";
-import { Request, Response, NextFunction } from "express";
 
 export interface MySequelizePool extends Pool {}
 export interface Connection extends MySequelizeConnection {}
@@ -30,29 +29,6 @@ export interface DatabaseInstance {
   releaseConnection: () => Promise<void>;
   terminate: () => Promise<void>;
 }
-
-/**
- * Middleware to attach TralseMySQL to requests.
- *
- * @param {import('mysql2/promise').Pool} pool - The database connection pool.
- * @param {string} dbName - The name of the database.
- * @param {boolean} [enableTransactions=false] - Whether to enable transaction support.
- * @returns {Function} - The middleware function.
- */
-export const TralseMySQL: (
-  pool: MySequelizePool,
-  dbName: string,
-  enableTransactions?: boolean
-) => (req: Request, res: Response, next: NextFunction) => Promise<void>;
-
-export const initializeDatabase: (
-  req: Request,
-  pool: MySequelizePool,
-  dbName: string,
-  enableTransactions: boolean
-) => Promise<DatabaseInstance>;
-
-export function log(dbName: string, message: string): void;
 
 declare module "express-serve-static-core" {
   interface Request {
